4060ec3c4e93cf99fdae4fc6371913b0ad5c3e96
itsmattpaw <itsmattpaw@gmail.com>
2021-09-24 20:04:35 -0400

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+    #if position is not equal to an X or O
+    if board[index] == "X" || board[index] == "O"
+        return true
+    else 
+        return false
+    end
+end
